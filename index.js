754222fcc873ca165d6b0a87574a540179a4b530
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-01-07 17:15:04 +0600

    package

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const port = process.env.PORT || 5002;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send('doctors portal server is running');
+})
+
+app.listen(port, () => console.log(`Doctors portal running on ${port}`))
